62bf5ea5331bda496cd67f2dc9e35d8f01786e78
Moe Nakajima <muskbergamot@gmail.com>
2023-03-31 12:30:34 +0900

    devise設定、モデル、コントローラ作成、ユーザー関連

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:mypage, :edit, :update]
+
+  def 
+    @user = current_user
+  end
+
+  def mypage
+    redirect_to user_path(current_user)
+  end
+
+  def edit
+
+  end
+
+  def update
+    if current_user.update(user_params)
+
+    else
+        redirect_to edit_user_path(current_user)
+    end
+  end
+end
