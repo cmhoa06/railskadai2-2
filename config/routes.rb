c9f35d4d7f4fcc46f4206177c07e21841fe7adec
Moe Nakajima <muskbergamot@gmail.com>
2023-04-03 19:25:55 +0900

    ユーザー関連

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'users/profile/edit/', to: 'users/profiles#edit'
+  get 'users/profile/show/', to: 'users/profiles#show'
+  get '/users/:id', to: 'users/accounts#show'
+  patch 'users/:id', to: 'users/profiles#update'
+  root to: "pages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_31_031808) do
+ActiveRecord::Schema.define(version: 2023_03_31_111550) do
+
+  create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
